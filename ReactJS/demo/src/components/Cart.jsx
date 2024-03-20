5ef7a5549909c4faa8c74af9b863da7b38c45212
Rabbitshah <maanavshah09@gmail.com>
2024-03-20 13:49:19 +0530

    commit three

--- ReactJS/demo/src/components/Cart.jsx
+++ ReactJS/demo/src/components/Cart.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import image from '../assets/Image1.jpg'
+// import image from '../assets/Image1.jpg'
 
 
-const Card = ({image,title,price,rating}) => {
-  return (
-      <div className=' h-screen w-screen flex flex-col items-center justify-center'>
+const Card = ({data}) => {
+    const {image, title, price, rating, keywords } = data;
+    return (
+        <div className=' h-screen p-5 flex flex-col items-center justify-center'>
         <div  className=' border border-black/100 p-5'>
             <div className='h-60 w-60'>
                 <img src={image} className='h-full w-full' alt="" />
@@ -12,7 +13,12 @@ const Card = ({image,title,price,rating}) => {
             <div className='my-5 text-3xl space-y-2'>
                 <p>{title}</p>
                 <p>{price}</p>
-                <p>rating {rating}</p>
+                <p>rating: {rating}</p>
+                <div>{keywords.map((i,index) =>(
+                    <div key={index}>
+                    <p>{i}</p>
+                    </div>
+                ))}</div>
             </div>
         </div>
     </div>
--- ReactJS/demo/src/screens/HomePage.jsx
+++ ReactJS/demo/src/screens/HomePage.jsx
@@ -2,14 +2,50 @@ import React from 'react'
 import image from '../assets/Image1.jpg'
 import Card from '../components/Cart'
 
+const data = [
+    {
+        id:1,
+        image: image,
+        title: "Hoodie",
+        price: "$50",
+        rating: "7",
+        keywords:[
+            "nasfjfdmfa",
+            "asdanifoalekdf"
+        ]
+    },
+    {
+        id:2,
+        image: image,
+        title: "Hoodie",
+        price:" $50",
+        rating: "7",
+        keywords:[
+            "nasfjfdmfa",
+            "asdanifoalekdf"
+        ]
+    },
+    {
+        id:3,
+        image: image,
+        title: "Hoodie",
+        price: "$50",
+        rating: "7",
+        keywords:[
+            "nasfjfdmfa",
+            "asdanifoalekdf"
+        ]
+    },
+]
+
 const HomePage = () => {
   return (
     <div className='flex flex-row'>
-        <Card image={image} title={"hiii"}/>
-        <Card image={image}/>
-        <Card image={image}/>
-        <Card image={image}/>
-        <Card image={image}/>
+        {data.map((item,index) => (
+            <div key={index}> 
+                <Card data={item}/>
+            </div>
+        ))}
     </div>
  
   )
